bf0a34bf8976442e2ac8a2b2ecc3c67d7ae73594
Carol Greene <carolgreene3@gmail.com>
2018-03-27 23:58:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
-  get "/:operation/:number1/:number2" do 
+  get "/:operation/:number1/:number2" do
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
@@ -44,8 +44,9 @@ class App < Sinatra::Base
     when "divide"
       "#{@number1 % @number2}"
     end
-      
-    
+  end
+
+
 
 
 end
