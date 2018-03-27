df4607f0cdc523d054e84195b31e73bba8a9e7d0
Carol Greene <carolgreene3@gmail.com>
2018-03-27 23:22:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,9 +16,9 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     number = params[:number].to_i
     @phrase = params[:phrase]
-    
-      "#{@phrase}" number.times do
-    end
+    number.times do
+      "#{@phrase}" 
+    end 
   end
 
 
