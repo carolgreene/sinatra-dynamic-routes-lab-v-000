eab7e50de2b80880afbe1b7b4b55405d902dff7c
Carol Greene <carolgreene3@gmail.com>
2018-03-28 00:03:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -34,8 +34,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    case @operation
-    when "add"
+
       "#{@number1 + @number2}"
 
     end
